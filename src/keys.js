77761185aadb5149447f399f600acf354e5c358d
FranciscoSMP <fmoralesp2001@gmail.com>
2025-05-04 18:23:44 -0600

    0.2

---
+++ src/keys.js
@@ -0,0 +1,23 @@
+const sql = require('mssql');
+
+const config = {
+    user: 'sa',
+    password: 'r62af79a',
+    server: 'DESKTOP-8QIU37S\\SQLEXPRESS',
+    //server: 'VIRTUALPC\\SQLEXPRESS',
+    database: 'Centro_Salud',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    },
+};
+
+const poolPromise = new sql.ConnectionPool(config)
+    .connect()
+    .then(pool => {
+        console.log('Conectado a la base de datos SQL Server');
+        return pool;
+    })
+    .catch(err => console.log('Error en la conexión a la base de datos: ', err));
+
+module.exports = { sql, poolPromise };
